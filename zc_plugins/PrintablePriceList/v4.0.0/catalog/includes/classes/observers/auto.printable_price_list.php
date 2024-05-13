fb1e3c12b479a44aa7633f2d9617b8f5ab66af92
lat9 <lat9@vinosdefrutastropicales.com>
2024-05-13 16:19:49 -0400

    #26: Deal with `.css` elements
    - Add observation at the end of the active template's `html_header.php` to inject the required (and overriddable) CSS files
    - Rename `stylesheet.css` to `profile-base.css`

--- zc_plugins/PrintablePriceList/v4.0.0/catalog/includes/classes/observers/auto.printable_price_list.php
+++ zc_plugins/PrintablePriceList/v4.0.0/catalog/includes/classes/observers/auto.printable_price_list.php
@@ -5,8 +5,38 @@ class zcObserverPrintablePriceList extends base
     {
         global $current_page_base;
 
-        if ($current_page_base === FILENAME_PRICELIST) {
-            require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'PrintablePriceList.php';
+        if ($current_page_base !== FILENAME_PRICELIST) {
+            return;
+        }
+
+        // -----
+        // Instantiate the price list for use by the template and observed notifications.
+        //
+        require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'PrintablePriceList.php';
+        global $price_list;
+        $price_list = new PrintablePriceList();
+
+        $this->attach(
+            $this,
+            [
+                'NOTIFY_HTML_HEAD_END',
+            ]
+        );
+    }
+
+    protected function updateNotifyHtmlHeadEnd(&$class, $eventId)
+    {
+        global $template, $price_list;
+
+        $ppl_base_css = $template->get_template_dir('profile-base.css', DIR_WS_TEMPLATE, FILENAME_PRICELIST, 'css') . 'profile-base.css';
+        if (file_exists($ppl_base_css)) {
+            echo '<link rel="stylesheet" href="' . $ppl_base_css . '">' . "\n";
+        }
+
+        $profile_css = 'profile-' . $price_list->getCurrentProfile() . '.css';
+        $ppl_profile_css = $template->get_template_dir($profile_css, DIR_WS_TEMPLATE, FILENAME_PRICELIST, 'css') . $profile_css;
+        if (file_exists($ppl_profile_css)) {
+            echo '<link rel="stylesheet" href="' . $ppl_profile_css . '">' . "\n";
         }
     }
 }
--- zc_plugins/PrintablePriceList/v4.0.0/catalog/includes/modules/pages/pricelist/header_php.php
+++ zc_plugins/PrintablePriceList/v4.0.0/catalog/includes/modules/pages/pricelist/header_php.php
@@ -6,18 +6,14 @@
  * @version v1.5.7 (or newer)
  */
 // -----
-// Part of the Printable Price List plugin for Zen Cart v1.5.7 and later.
-// Copyright (C) 2014-2021, Vinos de Frutas Tropicales (lat9)
+// Part of the Printable Price List plugin for Zen Cart v1.5.8a and later.
+// Copyright (C) 2014-2024, Vinos de Frutas Tropicales (lat9)
 //
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
 }
-require DIR_WS_MODULES . 'require_languages.php';
 
-// -----
-// Instantiate the price list for use by the template.
-//
-$price_list = new PrintablePriceList();
+require DIR_WS_MODULES . 'require_languages.php';
 
 $flag_disable_header = true;
 $flag_disable_footer = true;
