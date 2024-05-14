e113f95859794455ca1f4edfed1760787b5590b9
lat9 <lat9@vinosdefrutastropicales.com>
2024-05-14 19:51:35 -0400

    #26: Remove previous, non-encapsulated, files

--- includes/templates/template_default/pricelist/tpl_box_default.php
+++
@@ -1,6 +0,0 @@
-<?php
-// -----
-// Part of the "Printable Price List" plugin for Zen Cart.
-//
-// removed all makeup, since we don't wan't fancy boxes (languages and currencies) for the pricelist
-echo $content;
