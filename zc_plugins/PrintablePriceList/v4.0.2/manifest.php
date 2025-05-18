8df137d9eeb93a754eeee8df6368078f610671e4
lat9 <lat9@vinosdefrutastropicales.com>
2025-05-18 09:29:41 -0400

    Update zc_plugin version to 4.0.2

--- zc_plugins/PrintablePriceList/v4.0.2/manifest.php
+++ zc_plugins/PrintablePriceList/v4.0.2/manifest.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    'pluginVersion' => 'v4.0.1',
+    'pluginVersion' => 'v4.0.2',
     'pluginName' => 'Printable Price List',
     'pluginDescription' => 'This drop-in plugin provides a printable price-list for your Zen Cart store. Up to 3 separate variations can be customized; see the plugin\'s readme.html file for additional information.',
     'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
     'pluginId' => 173, // ID from Zen Cart forum
-    'zcVersions' => ['v158a', 'v200'],
+    'zcVersions' => ['v158a', 'v200', 'v210'],
     'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
     'github_repo' => 'https://github.com/lat9/printable_price_list', // url
     'pluginGroups' => [],
